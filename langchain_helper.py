83194305e1737b4fd75b191c6a11dd9a1a44cde9
Muskan Ravindra Gangwani <82218758+muskan-cloud@users.noreply.github.com>
2024-01-24 16:26:27 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -17,7 +17,7 @@ load_dotenv()  # take environment variables from .env (especially openai api key
 
 def get_few_shot_db_chain():
     db_user = "root"
-    db_password = "iitmmuskan"
+    db_password = " "
     db_host = "localhost"
     db_name = "atliq_tshirts"
     db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",
